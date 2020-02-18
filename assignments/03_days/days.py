066121a476efb97425211bce0980582f9e42834c
bnmcintyre <59971055+bnmcintyre@users.noreply.github.com>
2020-02-18 16:19:14 -0700

    Finished Assignment

--- assignments/03_days/days.py
+++ assignments/03_days/days.py
@@ -20,44 +20,43 @@ def get_args():
 
     parser.add_argument('day',
                         metavar='str',
-                        #nargs= '+',
+                        nargs='+',
                         help='What Day of the Week is it?')
 
     return parser.parse_args()
-
-#---------------------------------------------------
-def day_dict(args):
-    """gets day of the week"""
-
-    feeling = {'Monday': ' I never go to work',
-           'Tuesday': ' I stay at home',
-           'Wednesday': ' I never feel inclined',
-           'Thursday': ", it's a holiday",
-           'Friday': 's I detest',
-           'Saturday': "Oh, it's much too late on a",
-           'Sunday': ' is the day of rest'}
-
-    day = args.day
-    excuse = feeling.get(args.day)
-
-    if day in feeling:
-        if day[0] in 'MTW':
-            print(f'On {day}s{excuse}')
-        elif day[0:2] in 'FrSu':
-            print(f'And {day}{excuse}')
-        elif day[0:3] in 'Sat':
-            print(f'{excuse} {day}')
-    else:
-        day = (f'"{day}"')
-        print(f"Can't find {day}")
-
 
 # --------------------------------------------------
 def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    day_dict(args)
+
+    feeling = {
+        'Monday': ' I never go to work',
+        'Tuesday': ' I stay at home',
+        'Wednesday': ' I never feel inclined',
+        'Thursday': ", it's a holiday",
+        'Friday': 's I detest',
+        'Saturday': "Oh, it's much too late on a",
+        'Sunday': ' is the day of rest'}
+
+    day = args.day
+
+    for i in range(len(day)):
+        if day[i] in feeling:
+            if (day[i])[0:3] in 'MonTueWedThu':
+                day1 = day[i]
+                print(f'On {day1}s{feeling.get(day1)}')
+            elif (day[i])[0:2] in 'FrSu':
+                day1 = day[i]
+                print(f'And {day1}{feeling.get(day1)}')
+            elif (day[i])[0:3] in 'Sat':
+                day1 = day[i]
+                print(f'{feeling.get(day1)} {day1}')
+        else:
+            day1 = (f'"{day[i]}"')
+            print(f"Can't find {day1}")
+
 
 # --------------------------------------------------
 if __name__ == '__main__':
